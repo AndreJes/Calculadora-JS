b0bdc516e360f865b0f5ff42c1c3fc135fed494c
Andre JS <andrej.silva@outlook.com>
2020-02-10 18:20:22 -0300

    inicio dos scripts

---
+++ resources/scripts/calcController.js
@@ -0,0 +1,9 @@
+class Calculator {
+    constructor(){
+        this._numberOne = document.querySelector("#numberOneDisplay");
+        this._numberTwo = document.querySelector("#numberTwoDisplay");
+        this._lastNumber = "";
+        this._lastOperator = "";
+        this._result = "";
+    }
+}
